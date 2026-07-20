e04146490bba215e8130a52486d6aa4664635a5c
vaviliam-varsa <vaviliam.varsa@gmail.com>
2026-07-21 00:06:38 +0330

    Add AdvertisementCreateRequest DTO

---
+++ backend/src/main/java/com/example/secondhandbackend/dto/AdvertisementCreateRequest.java
@@ -0,0 +1,50 @@
+package com.example.secondhandbackend.dto;
+
+public class AdvertisementCreateRequest {
+
+    private String title;
+    private String description;
+    private Long price;
+    private Long categoryId;
+    private Long cityId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Long getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Long cityId) {
+        this.cityId = cityId;
+    }
+}
