1e29903d1239829c1a577e96f5ec3499d15d6781
vaviliam-varsa <vaviliam.varsa@gmail.com>
2026-07-20 23:41:45 +0330

    Add sample data for categories and cities

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories (name) VALUES ('Digital Goods');
+INSERT INTO categories (name) VALUES ('Home Appliances');
+INSERT INTO categories (name) VALUES ('Vehicles');
+INSERT INTO categories (name) VALUES ('Furniture');
+
+INSERT INTO cities (name) VALUES ('Tehran');
+INSERT INTO cities (name) VALUES ('Isfahan');
+INSERT INTO cities (name) VALUES ('Mashhad');
+INSERT INTO cities (name) VALUES ('Shiraz');
