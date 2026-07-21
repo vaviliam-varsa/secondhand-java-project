4b2c8547aea2c1bfe07e80196e7bdcb9092cfbb0
vaviliam-varsa <vaviliam.varsa@gmail.com>
2026-07-21 12:46:34 +0330

    Add SubmitRatingRequest DTO

---
+++ backend/src/main/java/com/example/secondhandbackend/dto/SubmitRatingRequest.java
@@ -0,0 +1,32 @@
+package com.example.secondhandbackend.dto;
+
+public class SubmitRatingRequest {
+
+    private Long advertisementId;
+    private Integer score;
+    private String comment;
+
+    public Long getAdvertisementId() {
+        return advertisementId;
+    }
+
+    public void setAdvertisementId(Long advertisementId) {
+        this.advertisementId = advertisementId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
