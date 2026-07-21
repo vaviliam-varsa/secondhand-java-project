2adaa58d0fc01508d2d1edc8856d77b50b36b36d
vaviliam-varsa <vaviliam.varsa@gmail.com>
2026-07-21 05:30:49 +0330

    Add SendMessageRequest DTO

---
+++ backend/src/main/java/com/example/secondhandbackend/dto/SendMessageRequest.java
@@ -0,0 +1,14 @@
+package com.example.secondhandbackend.dto;
+
+public class SendMessageRequest {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
