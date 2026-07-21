8b25c2b50dc436d9f09228e789e23981e4d01438
vaviliam-varsa <vaviliam.varsa@gmail.com>
2026-07-21 12:46:47 +0330

    Add InvalidInputException

---
+++ backend/src/main/java/com/example/secondhandbackend/exception/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.example.secondhandbackend.exception;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
